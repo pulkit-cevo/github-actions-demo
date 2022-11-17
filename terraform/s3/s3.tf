8071ec4f982399a63916da6420ad4a52697481f5
pulkit-cevo <pulkit.singh@cevo.com.au>
2022-11-17 16:05:55 +1100

    Update s3.tf

--- terraform/s3/s3.tf
+++ terraform/s3/s3.tf
@@ -1,5 +1,11 @@
+resource "random_string" "random_suffix" {
+  length  = 2
+  special = false
+  upper   = false
+}
+
 resource "aws_s3_bucket" "demo_bucket" {
-  bucket = "pulkit-github-action-$GITHUB_RUN_ID"
+  bucket = "pulkit-github-action-${random_string.random_suffix.result}"
 
   tags = {
     Name        = "demo_bucket"
